568960493a14289bdcb539143152c78dffabfb33
Bharathkumar123-K <karunakaran9344164779@gmail.com>
2025-07-09 10:03:45 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,9 @@
+public class First
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Bharath");
+        System.out.print("Hari Vasanth");
+        System.out.println("Kingstan");
+    }
+}
